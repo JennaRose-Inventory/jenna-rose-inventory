9ce70987e7f88b7b252ce02cf69418b1852ccefb
Kelvin <kelvinheng2822@gmail.com>
2026-05-22 19:19:27 +0800

    inventory app

---
+++ src/App_old.jsx
@@ -0,0 +1,9 @@
+```jsx
+export default function App() {
+  return (
+    <div>
+      <h1>Jenna Rose Inventory</h1>
+    </div>
+  );
+}
+```
---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+
+import {
+  getFirestore,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey:
+    "AIzaSyDj6OviWbUhE2280eOQ8N9BNh6hqHG4ekU",
+
+  authDomain:
+    "jenna-rose-inventory-4a4e3.firebaseapp.com",
+
+  projectId:
+    "jenna-rose-inventory-4a4e3",
+
+  storageBucket:
+    "jenna-rose-inventory-4a4e3.firebasestorage.app",
+
+  messagingSenderId:
+    "599844558666",
+
+  appId:
+    "1:599844558666:web:d789c6134000173e8393b3",
+};
+
+const app =
+  initializeApp(firebaseConfig);
+
+export const db =
+  getFirestore(app);
